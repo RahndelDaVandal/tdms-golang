502929e5d3702dbfb4909dc2e2aec9ae1862c218
Tim Fall <tfall@astra.com>
2022-08-25 21:35:21 -0700

    Formatting and lint
    Just a super fast linting/formatting cleanup pass

--- common/common.go
+++ common/common.go
@@ -2,67 +2,67 @@ package common
 
 import "fmt"
 
-type Property struct{
-  LenPropName uint32
-  Name string
-  Dtype uint32
-  LenValStr uint32
-  Value any
+type Property struct {
+	LenPropName uint32
+	Name        string
+	Dtype       uint32
+	LenValStr   uint32
+	Value       any
 }
-type Obj struct{
-  LenObjPath uint32
-  Name string
-  RawDataIndex uint32
-  NumOfProperties uint32
-  Properties []Property
+type Obj struct {
+	LenObjPath      uint32
+	Name            string
+	RawDataIndex    uint32
+	NumOfProperties uint32
+	Properties      []Property
 }
-type Segment struct{
-  Loc int
-  Header Header
-  NumOfObj uint32
-  Objs []Obj
+type Segment struct {
+	Loc      int
+	Header   Header
+	NumOfObj uint32
+	Objs     []Obj
 }
 type Segments []Segment
 
-func (s *Segments)Show(){
-  for _, v := range *s{
-    fmt.Printf("Segment Location: %v\n", v.Loc)
-    fmt.Printf("# of Objects: %v\n", v.NumOfObj)
-    for _, o := range v.Objs{
-      fmt.Printf("\tName: %v\n", o.Name)
-      fmt.Printf("\t\t# of Properties: %v\n", o.NumOfProperties)
-      for _, p := range o.Properties{
-        fmt.Printf("\t\t\t%v: %v\n", p.Name, p.Value)
-      }
-    } 
-  }
+func (s *Segments) Show() {
+	for _, v := range *s {
+		fmt.Printf("Segment Location: %v\n", v.Loc)
+		fmt.Printf("# of Objects: %v\n", v.NumOfObj)
+		for _, o := range v.Objs {
+			fmt.Printf("\tName: %v\n", o.Name)
+			fmt.Printf("\t\t# of Properties: %v\n", o.NumOfProperties)
+			for _, p := range o.Properties {
+				fmt.Printf("\t\t\t%v: %v\n", p.Name, p.Value)
+			}
+		}
+	}
 }
 
 type Headers []Header
 
-type Header struct{
-  Loc int64
-  Tag string
-  Toc uint32
-  Ver uint32
-  Seg uint64
-  Raw uint64
+type Header struct {
+	Loc int64
+	Tag string
+	Toc uint32
+	Ver uint32
+	Seg uint64
+	Raw uint64
 }
 
-func (lI *Header) Show(){
-  fmt.Printf("Loc: %v\n", lI.Loc)
-  fmt.Printf("Tag: %s\n", lI.Tag)
-  fmt.Printf("Toc: %v\n", lI.Toc)
-  fmt.Printf("Ver: %v\n", lI.Ver)
-  fmt.Printf("Seg: %v\n", lI.Seg)
-  fmt.Printf("raw: %v\n", lI.Raw)
+func (lI *Header) Show() {
+	fmt.Printf("Loc: %v\n", lI.Loc)
+	fmt.Printf("Tag: %s\n", lI.Tag)
+	fmt.Printf("Toc: %v\n", lI.Toc)
+	fmt.Printf("Ver: %v\n", lI.Ver)
+	fmt.Printf("Seg: %v\n", lI.Seg)
+	fmt.Printf("raw: %v\n", lI.Raw)
 }
 
 var ToC_Flag = map[uint32]string{
-  (1 << 2): "kTocNewObjList",       // 2
-  (1 << 3): "kTocRawData",          // 4
-  (1 << 5): "kTocInterleavedData",  // 8
-  (1 << 6): "kTocBigEndian",        // 32
-  (1 << 7): "kTocDAQmxRawData",     // 64
-  (1 << 1): "kTocMetaData",         // 128
+	(1 << 2): "kTocNewObjList",      // 2
+	(1 << 3): "kTocRawData",         // 4
+	(1 << 5): "kTocInterleavedData", // 8
+	(1 << 6): "kTocBigEndian",       // 32
+	(1 << 7): "kTocDAQmxRawData",    // 64
+	(1 << 1): "kTocMetaData",        // 128
 }
--- main.go
+++ main.go
@@ -16,100 +16,103 @@ import (
 // 20 00 00 00: tdsTypeString
 // 03 00 00 00: tdsTypeI32
 
-func main(){
-  fileName := "./test_files/2020-09-17T22-45-47_.tdms"
+func main() {
+	fileName := "./test_files/2020-09-17T22-45-47_.tdms"
 
-  r := reader.BytesReader{}
-  r.LoadFile(fileName)
+	r := reader.BytesReader{}
+	r.LoadFile(fileName)
 
-  S := common.Segments{}
-  s := common.Segment{}
-  var err error
+	S := common.Segments{}
+	s := common.Segment{}
+	var err error
 
-  for err == nil {
-    s, err = r.ReadSegment()
-    S = append(S, s)
-    r.Move(int64(s.Header.Seg))
-  }
+	for err == nil {
+		s, err = r.ReadSegment()
+		S = append(S, s)
+		r.Move(int64(s.Header.Seg))
+	}
 
-  fmt.Println("Number of Segments: ", len(S))
+	fmt.Println("Number of Segments: ", len(S))
 
-  for i:=0;i<11;i++{
-    fmt.Println(S[i])
-  }
+	for i := 0; i < 11; i++ {
+		fmt.Println(S[i])
+	}
 
-  defer r.File.Close()
+	defer r.File.Close()
 }
 
 // ---------------------------------------------------------------------------
 
-func loadFile(fileName string) *os.File{
-  file, openErr := os.Open(fileName)
-  if openErr != nil{
-    log.Fatalf("Failed to open %v", fileName)
-  }
-  return file
+func loadFile(fileName string) *os.File {
+	file, openErr := os.Open(fileName)
+	if openErr != nil {
+		log.Fatalf("Failed to open %v", fileName)
+	}
+	return file
 }
 
-type Window struct{
-  start int
-  end int
+type Window struct {
+	start int
+	end   int
 }
 
-func (w *Window) Move(size int){
-  w.start = w.end
-  w.end += size
+func (w *Window) Move(size int) {
+	w.start = w.end
+	w.end += size
 }
 
-func decodeBytes[T any](data []byte, field *T){
-  buffer := bytes.NewBuffer(data)
-  binary.Read(buffer, binary.LittleEndian, field)
+func decodeBytes[T any](data []byte, field *T) {
+	buffer := bytes.NewBuffer(data)
+	binary.Read(buffer, binary.LittleEndian, field)
 }
 
-func decodeBytesWithBuf[T any](buffer *bytes.Buffer, field *T){
-  binary.Read(buffer, binary.LittleEndian, field)
+func decodeBytesWithBuf[T any](buffer *bytes.Buffer, field *T) {
+	binary.Read(buffer, binary.LittleEndian, field)
 }
-
 
 func readNextBytes(file *os.File, number int) ([]byte, int, error) {
 	bytes := make([]byte, number)
 
 	bytesRead, err := file.Read(bytes)
 	if err != nil && err != io.EOF {
-    log.Fatal(err)
+		log.Fatal(err)
 	}
 
 	return bytes, bytesRead, err
 }
 
-func getSegmentHeaders(fileName string, headers *common.Headers){
-  f := loadFile(fileName)
-  fileStats, _ := f.Stat()
-  fileSize := fileStats.Size()
-  fmt.Printf("fileSize: %v\n", fileSize)
+func getSegmentHeaders(fileName string, headers *common.Headers) {
+	f := loadFile(fileName)
+	fileStats, _ := f.Stat()
+	fileSize := fileStats.Size()
+	fmt.Printf("fileSize: %v\n", fileSize)
 
-  h := common.Header{}
-  var loc int64
-  var data []byte
-  var err error
- 
-  // for loc < fileSize{
-  for {
-    loc, _ = f.Seek(int64(h.Seg), 1)
-    h.Loc = loc
-    data, _, err = readNextBytes(f, 4)
-    if err == io.EOF{break}
-    h.Tag = string(data)
-    data, _, err = readNextBytes(f, 24)
-    if err == io.EOF{break}
-    buffer := bytes.NewBuffer(data)
-    decodeBytesWithBuf(buffer, &h.Toc)
-    decodeBytesWithBuf(buffer, &h.Ver)
-    decodeBytesWithBuf(buffer, &h.Seg)
-    decodeBytesWithBuf(buffer, &h.Raw)
-    *headers = append(*headers, h)
-  }
-  fmt.Printf("Num of Segment Headers: %v\n", len(*headers))
+	h := common.Header{}
+	var loc int64
+	var data []byte
+	var err error
 
-  defer f.Close()
+	// for loc < fileSize{
+	for {
+		loc, _ = f.Seek(int64(h.Seg), 1)
+		h.Loc = loc
+		data, _, err = readNextBytes(f, 4)
+		if err == io.EOF {
+			break
+		}
+		h.Tag = string(data)
+		data, _, err = readNextBytes(f, 24)
+		if err == io.EOF {
+			break
+		}
+		buffer := bytes.NewBuffer(data)
+		decodeBytesWithBuf(buffer, &h.Toc)
+		decodeBytesWithBuf(buffer, &h.Ver)
+		decodeBytesWithBuf(buffer, &h.Seg)
+		decodeBytesWithBuf(buffer, &h.Raw)
+		*headers = append(*headers, h)
+	}
+	fmt.Printf("Num of Segment Headers: %v\n", len(*headers))
+
+	defer f.Close()
 }
